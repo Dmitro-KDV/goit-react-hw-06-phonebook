12ac0fef6d22acceebae35e5457b759f4f5b58e6
Dmitro-KDV <124252345+Dmitro-KDV@users.noreply.github.com>
2023-10-02 22:58:41 +0300

    com 4

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,8 +1,13 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer, FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-import {rootReducer} from './reducer'
+import {contactsReducer} from './reducer'
 
 // export const store = configureStore({
 //     reducer: {
@@ -10,19 +15,25 @@ import {rootReducer} from './reducer'
 //         filter: filtersReducer,
 //      },
 // })
-const middleware = getDefaultMiddleware({
-    immutableCheck: false,
-    serializableCheck: false,
-    thunk: true,
-  });
+// const middleware = getDefaultMiddleware({
+//     immutableCheck: false,
+//     serializableCheck: false,
+//     thunk: true,
+//   });
 
 const persistConfig = {
     key: 'root',
     storage,
 }
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, contactsReducer)
 
-export const store = configureStore({reducer: persistedReducer, middleware,})
+// export const store = configureStore({reducer: persistedReducer, middleware,})
+export const store = configureStore({reducer: {contacts:persistedReducer}, middleware: (getDefaultMiddleware) =>
+getDefaultMiddleware({
+  serializableCheck: {
+    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  },
+})})
 
 export const persistor = persistStore(store)
